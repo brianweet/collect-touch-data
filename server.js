63d9353f21d607b5f183a456183a0e8a2c760ff1
Brian Weeteling <brianweeteling@gmail.com>
2015-01-15 12:46:52 +0100

    Simple server, serve files from public folder

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-var http = require('http')
+/*var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
-}).listen(port);
+}).listen(port);*/
+
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+var port = process.env.PORT || 1337;
+
+// Serve up public folder
+var serve = serveStatic('public', {'index': ['index.html']})
+
+// Create server
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+
+// Listen
+server.listen(port)
