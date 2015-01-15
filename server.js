572abf5d204a6dfab40807853761dad5cfe3ef40
Brian Weeteling <brianweeteling@gmail.com>
2015-01-15 17:21:19 +0100

    Use express

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-/*var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);*/
+var express = require('express');
+var app = express();
 
-var finalhandler = require('finalhandler')
-var http = require('http')
-var serveStatic = require('serve-static')
+var oneDay = 86400000;
 
-var port = process.env.PORT || 1337;
+app.use(express.compress());
 
-// Serve up public folder
-var serve = serveStatic('public', {'index': ['index.html']})
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
-// Create server
-var server = http.createServer(function(req, res){
-  var done = finalhandler(req, res)
-  serve(req, res, done)
-})
-
-// Listen
-server.listen(port)
+app.listen(process.env.PORT || 3000);
