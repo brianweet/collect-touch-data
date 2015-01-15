53e2d8f3126dbfca6a73c7cd73e75b090781f1ff
Brian Weeteling <brianweeteling@gmail.com>
2015-01-15 17:52:49 +0100

    Can't return json or manifest files. Have to look into that later. Site doesn't work as an app neither.

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-var express = require('express');
-var app = express();
+/*var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);*/
 
-var oneDay = 86400000;
 
-app.use(express.compress());
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
 
-app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+var port = process.env.PORT || 1337;
 
-app.listen(process.env.PORT || 3000);
+// Serve up public folder
+var serve = serveStatic('public', {'index': ['index.html']})
+
+// Create server
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+
+// Listen
+server.listen(port)
+
+//var express = require('express');
+//var app = express();
+//
+//var oneDay = 86400000;
+//
+//app.use(express.compress());
+//
+//app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+//
+//app.listen(process.env.PORT || 3000);
